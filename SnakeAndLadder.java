13c07e6c0f393a975eddaf5e77eef792d6a9703f
Naveen2706 <naveen271996@gmail.com>
2021-06-23 08:17:55 +0530

    The player then checks for NoPlay,Ladder,Snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,15 +1,31 @@
-  
 class LadderSnake{
+public static void main(String[] args){
 	
-	public static void main(String[] args){
-		int position = 0;
-		System.out.println("Welcome to Snake-Ladder Program Problem");
-		System.out.println("You are at Starting Position::" + position);
-		Dice();
+	int position = 0;
+	System.out.println("position is " + position);
+	int die = (int) (Math.random() * 10) % 6 + 1;
+		System.out.println("dice value is " + die);
+	int value = (int) (Math.random() * 10) % 3 + 1;
+	System.out.println("case number is " + value);
+	 
+		switch(value) {
+	   case 1:
+	   System.out.println( "No Play You are in the same position." );
+	   break;
+	  
+	   case 2:
+	   position = (position + die);
+	   System.out.println( "your position after ladder is ::" + position );
+	   break;
+	   
+	   case 3: 
+	   position = (position - die);
+	  
+	  if(position<0){
+		  position = 0;
+	  }
+		 System.out.println( "Your Position after snake is::" + position );
+	   break;
+	   }
 	}
-
-	public static void Dice(){
-		int diceCheck = (int) Math.floor(Math.random() * 10) % 6+1;
-		System.out.println("Dice Value::"+ diceCheck);
-		}
 }
