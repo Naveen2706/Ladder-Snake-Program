49ff7356b20ffe3cb78aa963a8a5f338184ce6ea
Naveen2706 <naveen271996@gmail.com>
2021-06-23 07:54:33 +0530

    Position at Zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,8 @@
 class LadderSnake{
 	
 	public static void main(String[] args){
+		int position = 0;
 		System.out.println("Welcome to Snake-Ladder Program Problem");
+		System.out.println("You are at Starting Position::" + position);
 	}
 }
